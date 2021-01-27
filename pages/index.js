4964547e2045c80a22330984691bf9f28c456ded
Matheus Cabral <44450101+mcabralo@users.noreply.github.com>
2021-01-27 11:42:22 -0300

    Aula2 com criação do input e alteração de estado

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import styled from "styled-components";
-import db from "../db.json";
-import Widget from "../src/components/Widget";
-import Footer from "../src/components/Footer";
-import GitHubCorner from "../src/components/GitHubCorner";
-import QuizBackground from "../src/components/QuizBackground";
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+import db from '../db.json';
+// eslint-disable-next-line import/no-named-as-default
+import Widget from '../src/components/Widget';
+import Footer from '../src/components/Footer';
+import GitHubCorner from '../src/components/GitHubCorner';
+import QuizBackground from '../src/components/QuizBackground';
 
 // const BackgroundImage = styled.div`
 //   background-image: url(${db.bg});
@@ -24,15 +28,40 @@ export const QuizContainer = styled.div`
 `;
 
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>AluraQuiz - Modelo Base</title>
+      </Head>
       <QuizContainer>
         <Widget>
           <Widget.Header>
             <h1>The Legend of Zelda</h1>
           </Widget.Header>
           <Widget.Content>
-            <p>Lorem Ipsum amet</p>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              console.log('Fazendo uma Submissão');
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  console.log(name);
+                  // State
+                  setName(infosDoEvento.target.value);
+                  // name = infosDoEvento.target.value;
+                }}
+                placeholder="Diz seu nome aí"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
           </Widget.Content>
         </Widget>
         <Widget>
