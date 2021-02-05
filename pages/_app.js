32e2de8a8e84d2a38417e0e937c4e63edd36718b
Matheus Cabral <44450101+mcabralo@users.noreply.github.com>
2021-02-05 14:06:11 -0300

    Lottie

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <script src="https://unpkg.com/@lottiefiles/lottie-player@latest/dist/lottie-player.js" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
       </Head>
